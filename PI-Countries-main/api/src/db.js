946ed4b982838b74f6b741b5f3743c721b6a0ee6
Danilo <daniloud2809@gmail.com>
2022-11-07 17:37:10 -0500

    Prueba de Git

--- PI-Countries-main/api/src/db.js
+++ PI-Countries-main/api/src/db.js
@@ -39,3 +39,5 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
+
+// PRUEBA PARA COMPROBAR SI ESTE ES EL ARCHIVO XD
